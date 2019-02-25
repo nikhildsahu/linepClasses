2ec5b6f9d7f6b4f5513399ed7e5c2d1d78d6463a
Atharva Udapure <34984229+atharvau@users.noreply.github.com>
2019-02-25 07:11:13 +0530

    Update Fire.js

--- src/Fire.js
+++ src/Fire.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyBvNMTXVXuyqEOXzEuxUxQdrVacSo8FI6s",
-  authDomain: "lineup-db358.firebaseapp.com",
-  databaseURL: "https://lineup-db358.firebaseio.com",
-  projectId: "lineup-db358",
-  storageBucket: "lineup-db358.appspot.com",
-  messagingSenderId: "1026250129223"
+
 };
 let fire = firebase.initializeApp(config);
 export default fire;
